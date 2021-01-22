6d18b093b114022b4f413ae9477f9a329d712235
Mateusz <matyd356@gmail.com>
2021-01-22 14:15:29 +0100

    add 404 page component

---
+++ README.md
@@ -0,0 +1,1 @@
+//to do hash hasła
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+
 import Admin from '../Admin';
 import Home from '../Home'
 import Login from '../Login'
+import NotFound from '../404'
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -15,10 +17,12 @@ const App = () => {
       <Router>
         {/*TO DO implent auth routing in proper way*/}
         {/*TO DO implent browser history obj*/}
-        {user ? <Redirect to='/home' /> : <Redirect to='/' />}
-        <Route exact path={ROUTES.LOGIN} component={() => <Login setUser={setUser} />} />
-        <Route path={ROUTES.HOME} component={() => <Home user={user} />} />
-        <Route path={ROUTES.ADMIN} component={() => <Admin user={user} />} />
+        <Switch>
+          <Route exact path={ROUTES.LOGIN} component={() => <Login setUser={setUser} />} />
+          <Route exact path={ROUTES.HOME} component={() => <Home user={user} />} />
+          <Route exact path={ROUTES.ADMIN} component={() => <Admin user={user} />} />
+          <Route path='/' component={NotFound} />
+        </Switch>
       </Router>
     </div>)
 };
